98bca2df68c0c544201c6a25048e5d7f95b6971c
maneusfreitas <202100104@estudantes.ips.pt>
2024-03-18 00:49:39 +0000

    Delete All Conflicts

--- ApolloStage/Helper/IHttpClientHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApolloStage
-{
-    public interface IHttpClientHelper
-    {
-        Task<string> SendAysnc(string uri, string token);
-        Task<string> PostAsync(string uri, IEnumerable<KeyValuePair<string, string>> content);
-
-    }
-}
